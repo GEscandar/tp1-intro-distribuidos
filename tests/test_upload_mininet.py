271b22424cae60bb6f857764c2f015376d1251db
antopell <antopell29@gmail.com>
2024-09-23 10:54:22 -0300

    Cambio como creo la red para que funcione el test Tenemos test con perdida!!

--- tests/test_upload_mininet.py
+++ tests/test_upload_mininet.py
@@ -11,22 +11,19 @@ from mininet.net import Mininet
 from mininet.topo import Topo
 
 
-# codigo de: https://github.com/mininet/mininet/wiki/Introduction-to-Mininet#limits
 class SingleSwitchTopo(Topo):
-    def build(self, n=2):
-        switch = self.addSwitch('s1')
-        # Python's range(N) generates 0..N-1
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            self.addLink(host, switch, loss=10)
+    def __init__(self):
+        Topo.__init__(self)
+        # Add hosts and switches
+        server = self.addHost('h1')
+        cliente1 = self.addHost('h2')
 
+        # Add links
+        self.addLink(server, cliente1, loss=10)
 
-def basic_server(server, server_command):
-    logging.info("Starting server")
-    return server.cmd(server_command)
 
 def upload(port, filepath: Path):
-    topo = SingleSwitchTopo(2)
+    topo = SingleSwitchTopo()
     net = Mininet(topo=topo, controller=None)
     server = net.get('h1')
     client = net.get('h2')
@@ -37,21 +34,19 @@ def upload(port, filepath: Path):
     
     logging.info(server_command)
     logging.info(client_command)
-    logging.info(client)
     created_file = Path(filepath.name)
-    t = threading.Thread(target=basic_server, args=[server, server_command])
-    try:
-        
-        t.start()
-        res = client.cmd(client_command)
+    try: 
+        server.sendCmd(server_command)
+        logging.info("Starting client")
+
+        client.cmd(client_command)
         logging.info("Finished client")
         time.sleep(0.1)  # wait till the server saves the file
-        assert res == True
+        server.terminate()
+
         assert created_file.exists()
         assert created_file.stat().st_size == filepath.stat().st_size
     finally:
-        if t.is_alive():
-            t.join()
         if created_file.exists():
             created_file.unlink()
         net.stop()
@@ -63,7 +58,7 @@ def test_upload_small_file_mininet():
     upload(port, filepath)
 
 
-# def test_upload_medium_small_file_mininet():
-#     addr = ("localhost", 23457)
-#     filepath = Path("tests", "files", "medium_small.txt")
-#     upload(addr, filepath)
+def test_upload_medium_small_file_mininet():
+    port = 23458
+    filepath = Path("tests", "files", "medium_small.txt")
+    upload(port, filepath)
