b1eeeaa5e506f098eb0c050acafe2e09260f8ae5
Iván Grzegorczyk <ivang@pagotic.com>
2024-10-02 03:16:39 -0300

    Agrego un script para hacer pruebas con varios clientes y mininet.

--- mininet_prueba.py
+++ mininet_prueba.py
@@ -1,51 +1,64 @@
-# from mininet.topo import Topo
-# from mininet.net import Mininet
-# from mininet.util import dumpNodeConnections
-# from mininet.log import setLogLevel
+#!/usr/bin/python
+import argparse
+import time
 
-# from pathlib import Path
-
-
-# class SingleSwitchTopo(Topo):
-#     def __init__(self):
-#         Topo.__init__(self)
-#         # Add hosts and switches
-#         server = self.addHost("h1")
-#         cliente1 = self.addHost("h2")
-
-#         # Add links
-#         self.addLink(server, cliente1, loss=10)
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.net import Mininet
+from mininet.node import OVSController
 
 
-# def simpleTest():
-#     "Create and test a simple network"
-#     topo = SingleSwitchTopo()
-#     net = Mininet(topo=topo, controller=None)
-#     net.start()
-#     print("Dumping host connections")
-#     dumpNodeConnections(net.hosts)
+def setup_topology(number_of_clients, packet_loss):
+    net = Mininet(controller=OVSController, link=TCLink)
 
-#     server = net.get("h1")
-#     client = net.get("h2")
+    info("*** Adding controller\n")
+    net.addController("c0")
 
-#     port = 23457
-#     filepath = Path("tests", "files", "small.txt")
+    info("*** Adding switch\n")
+    s1 = net.addSwitch("s1")
 
-#     server_command = (
-#         f"venv/bin/python src/server -s server_storage -H {server.IP()} -p {port}"
-#     )
-#     client_command = f"venv/bin/python src/upload -s {filepath.absolute()} -n {filepath.name} -q -H {server.IP()} -p {port}"
+    number_of_hosts = number_of_clients + 1
+    hosts = [
+        net.addHost(f"h{i}", ip=f"10.0.0.{i}") for i in range(1, number_of_hosts + 1)
+    ]
 
-#     server.sendCmd(server_command)
-#     print("Starting client")
+    for i in range(0, number_of_hosts):
+        net.addLink(hosts[i], s1, loss=packet_loss)
 
-#     client.cmdPrint(client_command)
-#     print("Finished client")
-#     server.terminate()
-#     net.stop()
+    info("*** Starting network\n")
+    net.start()
+
+    port = 4000
+
+    info("*** Starting server on h1\n")
+    hosts[0].cmd(
+        f"venv/bin/python src/server -p {port} -s server_storage -H {hosts[0].IP()} > server.log 2>&1 &"
+    )
+
+    time.sleep(1)
+
+    info("*** Starting clients in background\n")
+
+    for i in range(1, number_of_hosts):
+        hosts[i].cmd(
+            f"venv/bin/python src/download -n medium.txt -d m{i}.txt -H {hosts[0].IP()} -p {port} > client{i}.log 2>&1 &"
+        )
+
+    CLI(net)
+
+    info("*** Stopping network\n")
+    net.stop()
 
 
-# if __name__ == "__main__":
-#     # Tell mininet to print useful information
-#     setLogLevel("info")
-#     simpleTest()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Mininet topology setup")
+    parser.add_argument("--num-clients", type=int, default=3, help="Number of clients")
+    parser.add_argument(
+        "--packet-loss", type=int, default=10, help="Packet loss percentage"
+    )
+
+    args = parser.parse_args()
+
+    setLogLevel("info")
+    setup_topology(args.num_clients, args.packet_loss)
